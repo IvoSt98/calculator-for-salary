0d9369e09f72973d5d2b772df6ce432cc7c91ca4
Ivo Stoykov <ivo.stoykov@abv.bg>
2024-07-30 15:56:01 +0000

    Create start def

--- run.py
+++ run.py
@@ -1,1 +1,19 @@
 from tabulate import tabulate
+
+def start():
+
+    """ 
+    The function expalin how the program it's working and for what it is.
+    """
+   
+    print("     Welcome to Your Salary/Income Calculator!\n     ")
+    print("     The 40/40/20 System:\n      ")
+    print(" First, enter your income for a month, which will be divided as follows: ")
+    print(" 40 percentages - needs, 40 percentages - savings/investments, and 20 percentages - living expenses.\n   ")
+    print("-Example for NEEDS: this is your insuarance for the car, bills, credits, food and etc. The things without you can't live. ")
+    print("-Example for SAVINGS/INVESTMENTS: this is where you will write the money for education, for your holiday, investments in stocks and etc.")
+    print("-Example for LIVING EXPENSES: here you need to fill your money for cigaretes, parties, restaurants and etc. Something without you can survive\n")
+    print(" Next, add your expenses by selecting a category and entering the purpose and amount. ")
+    print(" In the end, you'll know if you're managing your finances well.\n ")
+    print("     Good Luck!\n        ")
+start()
