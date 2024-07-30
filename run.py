121d414fc481401c17118275c35a24181e597013
Ivo Stoykov <ivo.stoykov@abv.bg>
2024-07-30 16:02:48 +0000

    Create 3 defs to get the income to calculate it by % adn after that to show it in a table

--- run.py
+++ run.py
@@ -16,4 +16,43 @@ def start():
     print(" Next, add your expenses by selecting a category and entering the purpose and amount. ")
     print(" In the end, you'll know if you're managing your finances well.\n ")
     print("     Good Luck!\n        ")
-start()
+
+def get_income():
+    
+    """ 
+    The function calculate the sum to be always float, if it's a string it'll be 
+    displayed error, if it's less then 4 digits it will print again error message
+    """
+
+    while True: 
+        try:
+            user_income = input("Enter your income (at least 4 digits): ")
+            income = float(user_income)
+            if income >= 1000:  
+                return income
+            else:
+                print("Income must be at least 4 digits. Please try again.")
+        except ValueError:
+            print("Invalid data. Please enter a number with at least 4 digits.")
+
+def calculate_income(income):
+
+    """ 
+    The def calculate the month income in % for needs, savings
+    and living
+    """
+
+    needs = 0.4 * income
+    savings = 0.4 * income
+    living = 0.2 * income
+    return needs, savings, living
+
+def build_table(needs, savings, living):
+    table = ["Needs", "Savings/Investments", "Living Expenses"]
+    info_income = [[f"${needs}", f"${savings}", f"${living}"]]
+    print(tabulate(info_income, headers=table, tablefmt="simple"))
+
+#start()
+income = get_income()
+needs, savings, living = calculate_income(income)
+build_table(needs, savings, living)
